3daaa8dda76fd5e52f96d4f29cd1cc5b09edf8f8
Bryce171717 <brice.jamain17@gmail.com>
2024-06-15 11:04:10 +0200

    envoi main v1

---
+++ main.tf
@@ -0,0 +1,13 @@
+# main.tf
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "chatbot_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "chatbotjunior-backend"
+  }
+}
